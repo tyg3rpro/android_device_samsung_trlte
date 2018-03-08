7b43d8748a2eabaf0e749521e9291ffe7fe3ff91
micky387 <mickaelsaibi@free.fr>
2018-03-08 22:21:09 +0100

    audio: copy mixer_path to /vendor for O

--- releasetools/mixer_paths_eur.sh
+++ releasetools/mixer_paths_eur.sh
@@ -3,6 +3,5 @@
 MIXER_PATHS=/tmp/install/bin/mixer_paths_eur.xml
 
 if [ "$VARIANT" = "xx" ] ; then
-    cp -f "$MIXER_PATHS" "/system/etc/mixer_paths.xml"
+    cp -f "$MIXER_PATHS" "/system/vendor/etc/mixer_paths.xml"
 fi
-

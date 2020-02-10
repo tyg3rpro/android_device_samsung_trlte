17b707457868fb84df7aba1f64a06f0e637b805c
tyg3rpro <32470566+tyg3rpro@users.noreply.github.com>
2020-02-10 17:24:41 -0500

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,42 +1,88 @@
-#
-# Copyright (C) 2017 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+DEVICE_TREE := device/samsung/trlte
 
-# inherit from common trlte
--include device/samsung/trlte-common/BoardConfigCommon.mk
+CM_PLATFORM_SDK_VERSION := 7	# Required for libf2fs.so
+override TARGET_OUT_VENDOR_SHARED_LIBRARIES = $(TARGET_OUT_SHARED_LIBRARIES)
 
-TARGET_OTA_ASSERT_DEVICE := trlte,trltedt,trltecan,trltespr,trltetmo,trlteusc,trltevzw,trltexx
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := APQ8084
 
-# Camera
-TARGET_FIXUP_PREVIEW := true
+# Platform
+TARGET_BOARD_PLATFORM := apq8084
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno420
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := krait
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/apq8084
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
+TARGET_KERNEL_CONFIG := twrp_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := apq8084_sec_trlte_eur_defconfig
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
 
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_apq8084
-TARGET_LIBINIT_APQ8084_DEFINES_FILE := device/samsung/trlte/init/init_trlte.cpp
+# Boot image
+BOARD_KERNEL_CMDLINE := console=tty0 androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive user_debug=23 msm_rtb.filter=0x3b7 dwc3_msm.cpu_to_affin=1
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02600000 --tags_offset 0x02400000 --board SYSMAGIC000RU
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_TREE)/mkbootimg.mk
 
 # Partitions
-BOARD_FLASH_BLOCK_SIZE := 262144
-BOARD_BOOTIMAGE_PARTITION_SIZE := 17825792
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 19932160
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3774873600
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 27040657408
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x001100000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x001300000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x0E1000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x62EFF7000 #0x62EFFB000 - 16384 (footer)
+BOARD_FLASH_BLOCK_SIZE := 0x40000
 
-# Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/trlte-common/releasetools
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_SUPPRESS_SECURE_ERASE := true
 
-# inherit from the proprietary version
--include vendor/samsung/trlte/BoardConfigVendor.mk
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/f9200000.ssusb/f9200000.dwc3/gadget/lun%d/file"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 162
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INCLUDE_NTFS_3G := true
+# exFAT drivers included in the kernel
+TW_NO_EXFAT_FUSE := true
+# No love for the wicked (device ships with M)
+TW_EXCLUDE_SUPERSU := true
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
+TW_CRYPTO_SYSTEM_VOLD_DEBUG := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# Debug flags
+# TWRP_INCLUDE_LOGCAT := true
+
+# Init properties from bootloader version, ex. model info
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_trlte
+TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_TREE)/init/init_trlte.cpp
+
+# Oreo
+TW_NO_LEGACY_PROPS := true
